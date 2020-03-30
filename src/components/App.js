bcb9f692fa4ee7a4a742a68ce17ef98aded52c5a
Donald Wu <yeukfei02@gmail.com>
2020-03-30 17:00:33 +0800

    feature/create-project-structure-#1: update background color

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import Normalize from 'react-normalize';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { createMuiTheme } from "@material-ui/core/styles";
 
 import MainPage from './mainPage/MainPage';
 
+const theme = createMuiTheme({
+  palette: {
+    background: {
+      default: "lightblue"
+    }
+  }
+});
+
 function App() {
   return (
-    <div>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
       <Normalize />
       <MainPage />
-    </div>
+    </MuiThemeProvider>
   );
 }
 
