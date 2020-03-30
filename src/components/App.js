dc4ddf92534bf032bfa5d3816865ba61cda70e27
Donald Wu <yeukfei02@gmail.com>
2020-03-30 22:52:42 +0800

    fix-code: remove warning in console

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Normalize from 'react-normalize';
 import CssBaseline from "@material-ui/core/CssBaseline";
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider as MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import MainPage from './mainPage/MainPage';
 
